1d39000857499567b9183875ed5ccd577dd42c0a
medilies <medilies@gmail.com>
2022-06-15 21:01:51 +0100

    Upgrade Jigsaw:Tailwind-3 using 'fast-glob.sync'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 module.exports = {
-  purge: [
-    'source/**/*.blade.php',
-    'source/**/*.md',
-    'source/**/*.html',
-  ],
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    content: require("fast-glob").sync([
+        "source/**/*.blade.php",
+        "source/**/*.md",
+        "source/**/*.html",
+    ]),
+    theme: {
+        extend: {
+            colors: {
+                "primary-1": "#103778",
+                "primary-2": "#151F30",
+                "secondary-1": "#FF7A48",
+                "secondary-2": "#E3371E",
+                terciary: "#0593A2",
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 };
--- webpack.mix.js
+++ webpack.mix.js
@@ -6,10 +6,7 @@ mix.setPublicPath("source/assets/build");
 
 mix.jigsaw()
     .js("source/_assets/js/main.js", "js")
-    .css("source/_assets/css/main.css", "css", [
-        require("postcss-import"),
-        require("tailwindcss"),
-    ])
+    .postCss("source/_assets/css/main.css", "css", [require("tailwindcss")])
     .options({
         processCssUrls: false,
     })
