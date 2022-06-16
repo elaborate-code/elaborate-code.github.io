7daeca6a88675971521eabcbf6242435d74dd1fd
medilies <medilies@gmail.com>
2022-06-16 22:48:35 +0100

    Aborted particles.js experiment

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,9 +1,1 @@
-import "particles.js";
 
-particlesJS.load(
-    "particles-js",
-    "assets/build/json/particles.json",
-    function () {
-        console.log("callback - particles.js config loaded");
-    }
-);
