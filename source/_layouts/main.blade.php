06e079bad70d9e70f78682b0e908ca906ade8c57
medilies <medilies@gmail.com>
2022-06-18 22:28:03 +0100

    og tags

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -20,9 +20,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="canonical" href="{{ $page->getUrl() }}">
-    <meta name="description" content="{{ $page->description }}">
-    <title>{{ $page->title }}</title>
+    <meta property="og:url" content="{{ $page->getUrl() }}" />
 
+    <title> {{ $page->title }} </title>
+    <meta property="og:title" content="{{ $page->title }}" />
+
+    <meta name="description" content="{{ $page->description }}">
+    <meta property="og:description" content="{{ $page->description }}" />
+
+    <meta property="og:type" content="website" />
+
+    <meta property="og:image"
+        content="assets/images/cloud-storage-background-remixed-from-public-domain-by-nasa-min.jpg" />
+
+    <meta property="og:locale" content="en_US" />
+
+    {{-- scripts --}}
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
 
@@ -35,13 +48,17 @@
 
 
     <link rel="manifest" href="/manifest.json">
-    {{--  --}}
+
+    {{-- icons --}}
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicons/favicon-16x16.png">
     <link rel="mask-icon" href="/assets/favicons/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
+    <meta name="theme-color" content="#F22929">
+
+    {{--  --}}
+    <meta name="twitter:card" content="summary_large_image" />
 </head>
 
 <body class="font-raleway">
