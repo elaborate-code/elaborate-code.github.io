2145f89ac659060e619cf84f712bf6d4b9f877f7
medilies <medilies@gmail.com>
2022-06-18 13:10:37 +0100

    CTA + who are we sections

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,7 +4,7 @@
     <section class="h-screen hero-bg sm:hero-bg-alt text-white">
         <div class="container h-full m-auto flex flex-col">
 
-            <nav class="h-20 bg-white opacity-10"></nav>
+            <nav class="h-20 "></nav>
 
             <header class="flex-1 flex flex-col justify-center">
 
@@ -33,7 +33,18 @@
 
     {{-- <section class="bg-gray-100 h-24"> our partners </section> --}}
 
-    <section class="py-32 md:py-16 px-12 bg-white">
+    <section class="py-16 px-12 bg-white-smoke">
+
+        <h2 class="mb-6 text-bloodmyst-isle text-4xl font-bold font-yeseva-one text-center"> Who are we</h2>
+
+        <p class="w-96 mx-auto text-center">
+            We are a team of passionate IT people, we mainly craft on-demand web APPs (CMS, ERP, CRM, IOT, E-Commerce ...)
+            and websites. We also offer infrastructure upgrade services like setting up Windows/Linux server with its
+            services and optimize networks physically and logically
+        </p>
+    </section>
+
+    <section class="py-10 md:py-16 px-12 bg-white">
 
         <h2 class="mb-12 text-nero text-4xl font-bold font-yeseva-one text-center"> Our services </h2>
 
@@ -43,5 +54,30 @@
 
     </section>
 
+    <section class="bg-bloodmyst-isle-dark py-10 px-4">
+        <h2 class="mb-4 text-bloodmyst-isle text-4xl font-bold font-merriweather text-center"> Are you intrested in our
+            services ? </h2>
+
+        <p class="mb-2 text-white text-center">
+            Let us know your E-mail or phone number and we will reach out to you
+        </p>
+
+        <form method="post">
+            <div class="flex justify-center items-stretch gap-4 text-lg">
+                <input type="text" class="w-72 px-4 outline-none border-none rounded-sm">
+
+                <button
+                    class="px-16 py-4 bg-bloodmyst-isle text-frozen-blue-50 font-bold rounded-sm shadow-sm hover:shadow-white-smoke">
+                    Send </button>
+            </div>
+        </form>
+    </section>
+
     <section class="bg-frozen-blue-50 h-96"></section>
+
+    <footer class="bg-nero">
+        <div class="py-4 text-white-smoke text-center">
+            Copyright &copy; {{ date('Y') }}
+        </div>
+    </footer>
 @endsection
