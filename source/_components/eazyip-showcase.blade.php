adc272e4e2eb748f533ab659ca3ed185359d23b3
medilies <medilies@gmail.com>
2022-06-20 21:21:19 +0100

    Fixed typo and touched EazyIP showcase div style

--- source/_components/eazyip-showcase.blade.php
+++ source/_components/eazyip-showcase.blade.php
@@ -1,18 +1,19 @@
 <div class="bg-frozen-blue-50">
     <div class="container mx-auto p-2 lg:p-8">
 
-        <h3 class="text-frozen-blue-dark text-2xl font-merriweather font-bold text-center">
+        <h3 class="text-frozen-blue-dark text-2xl font-merriweather font-bold text-center underline">
             Eazy IP
         </h3>
 
-        <div class="w-full mx-auto p-2 sm:w-[75%] lg:w-[640px] md:mx-auto">
+        <a href="https://eazyip.github.io/" target="__blank"
+            class="block w-full mx-auto p-2 sm:w-[75%] lg:w-[640px] md:mx-auto hover:rotate-2 transition ease-in-out delay-75 duration-150">
             <img src="assets/images/eazyip-ladingpage-mockups-min.png" alt="Eazy IP Web app showcase mockup" srcset=""
-                class="object-fill shadow-lg">
-        </div>
+                class="object-fill shadow-md">
+        </a>
 
         <p class="max-w-[720px] mx-auto text-center">
-            Eazy IP is a Progressive Web App that aims at becoming an essensial toolkit for the network engeneers by the
-            IP operations flawlessly
+            Eazy IP is a Progressive Web App that aims at becoming an essensial toolkit for the network engineers by the
+            handling IP operations flawlessly
         </p>
     </div>
 </div>
