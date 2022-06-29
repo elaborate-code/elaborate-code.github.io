99254726fa865c594ad703bef3a0238fb0dabfc7
medilies <medilies@gmail.com>
2022-06-29 19:18:44 +0100

    FEAT: langs reside in JSON files

--- bootstrap.php
+++ bootstrap.php
@@ -5,13 +5,42 @@ use TightenCo\Jigsaw\Jigsaw;
 /** @var $container \Illuminate\Container\Container */
 /** @var $events \TightenCo\Jigsaw\Events\EventBus */
 
-/**
- * You can run custom code at different stages of the build process by
- * listening to the 'beforeBuild', 'afterCollections', and 'afterBuild' events.
- *
- * For example:
- *
- * $events->beforeBuild(function (Jigsaw $jigsaw) {
- *     // Your code here
- * });
- */
+$events->beforeBuild(function (Jigsaw $jigsaw) {
+
+    // Be aware of the languages added by the dev
+    $site_langs = $jigsaw->getConfig('site_langs');
+
+    foreach ($site_langs as $site_lang) {
+
+        // set path to lang folder
+        $lang_folder = lang_folder_realpath($site_lang);
+
+        $translations = $jigsaw->getConfig($site_lang)?->toArray() ?? [];
+
+        foreach (scandir($lang_folder) as $lang_file) {
+
+            if (is_not_json($lang_file))
+                continue;
+
+            // push the loaded translations
+            $translations += decoded_lang_json("$lang_folder/$lang_file");
+        }
+
+        $jigsaw->setConfig($site_lang, $translations);
+    }
+});
+
+function lang_folder_realpath(string $lang_code): string
+{
+    return realpath(__DIR__ . "/lang/$lang_code");
+}
+
+function is_not_json(string $path): bool
+{
+    return substr($path, -5) !== ".json";
+}
+
+function decoded_lang_json(string $abs_path): array
+{
+    return json_decode(file_get_contents($abs_path), true);
+}
--- config.php
+++ config.php
@@ -8,6 +8,8 @@ return [
     'description' => 'IT Services and IT Consulting',
     'collections' => [],
     // 
+    'site_langs' => ['en', 'fr'],
+    // 
     'routes' => [
         ['url' => '/', 'name' => 'Home',],
         ['url' => '/contact', 'name' => 'Contact',],
@@ -71,74 +73,5 @@ return [
             'href' => '#',
             'img' => '/assets/images/residenceely.png',
         ],
-    ],
-    // =========================================================================================================
-    'en' => [
-        'Send' => 'Send',
-        'Phone number' => 'Phone number',
-        'Full name' => 'Full name',
-        // Hero
-        'Achieve more by adopting automated workflows and networking informations between the various segments of your company' => 'Achieve more by adopting automated workflows and networking informations between the various
-        segments of your company',
-        'Request a service' => 'Request a service',
-        // Who are we
-        'Who are we' => 'Who are we',
-        'We are a team of passionate IT people, we mainly craft on-demand web APPs (CMS, ERP, CRM, IOT, E-Commerce ...) and websites. We also offer infrastructure upgrade services like setting up Windows/Linux server with its tools and optimize networks physically and logically.' => 'We are a team of passionate IT people, we mainly craft on-demand web APPs (CMS, ERP, CRM, IOT, E-Commerce ...) and websites. We also offer infrastructure upgrade services like setting up Windows/Linux server with its tools and optimize networks physically and logically.',
-        // Services
-        'Our services' => 'Our services',
-        'Software development' => 'Software development',
-        'We believe we got top notch programmers  that are ready to realise your ideas and eradicate points of failure within your company' => 'We believe we got top notch programmers  that are ready to realise your ideas and eradicate points of failure within your company',
-        'Systems administration' => 'Systems administration',
-        'Linux or Windows? whatever preference you got we will provision the right server with the right tools to your company' => 'Linux or Windows? whatever preference you got we will provision the right server with the right tools to your company',
-        'Network engineering' => 'Network engineering',
-        'We know very well that the network is the foundation of digitalisation and you can trust us to manage its various aspects.' => 'We know very well that the network is the foundation of digitalisation and you can trust us to manage its various aspects.',
-        // CTA
-        'Are you interested in our services ?' => 'Are you interested in our services ?',
-        'Let us know your E-mail and phone number and we will reach out to you shortly' => 'Let us know your E-mail and phone number and we will reach out to you shortly',
-        'Or send us a detailed message' => 'Or send us a detailed message',
-        // projects
-        'Our Featured applications' => 'Our Featured applications',
-        'A Progressive Web App that aims at becoming an essential toolkit for the network engineers by handling the IP operations flawlessly' => 'A Progressive Web App that aims at becoming an essential toolkit for the network engineers by handling the IP operations flawlessly',
-        'A multi-user home automation IOT platform that enables functionalities like realtime sensors monitoring and remote actions' => 'A multi-user home automation IOT platform that enables functionalities like realtime sensors monitoring and remote actions',
-        'Tailored according to the algerian education system and destined to be in charge of administering students registrations step by step in privet schools' => 'Tailored according to the algerian education system and destined to be in charge of administering students registrations step by step in privet schools',
-        'Helps real estate agencies keep records of their apartments and clients and fully manages the sales process' => 'Helps real estate agencies keep records of their apartments and clients and fully manages the sales process',
-        // contact page
-        "Contact us" => "Contact us",
-        "Let's talk" => "Let's talk",
-        "Let us know how we can help and we'll get in touch with you shortly" => "Let us know how we can help and we'll get in touch with you shortly",
-    ],
-    // =========================================================================================================
-    'fr' => [
-        'Send' => 'Envoyer',
-        'Phone number' => 'Numéro de téléphone',
-        'Full name' => 'Nom complet',
-        // Hero
-        'Achieve more by adopting automated workflows and networking informations between the various segments of your company' => 'Achevez plus en adoptant un mode de travail automatisés et en circulant vos informations sous réseau entre les différents segments de votre entreprise',
-        'Request a service' => 'Demandez un service',
-        // Who are we
-        'Who are we' => 'Qui sommes nous',
-        'We are a team of passionate IT people, we mainly craft on-demand web APPs (CMS, ERP, CRM, IOT, E-Commerce ...) and websites. We also offer infrastructure upgrade services like setting up Windows/Linux server with its tools and optimize networks physically and logically.' => 'Nous sommes une équipe d\'informaticiens passionnés, nous créons principalement des applications web à la demande (CMS, ERP, CRM, IOT, E-Commerce ...) et des sites web. Nous offrons également des services de mise à niveau de l\'infrastructure comme la configuration du serveur Windows/Linux avec ses outils et l\'optimisation physique et logique des réseaux.',
-        // Services
-        'Our services' => 'Nos services',
-        'Software development' => 'Développement de logiciels',
-        'We believe we got top notch programmers  that are ready to realise your ideas and eradicate points of failure within your company' => 'Nous croyons que nous avons des programmeurs de premier ordre qui sont prêts à concrétiser vos idées et à éliminer les points de défaillance au sein de votre entreprise',
-        'Systems administration' => 'Administration des systèmes',
-        'Linux or Windows? whatever preference you got we will provision the right server with the right tools to your company' => 'Linux ou Windows ? quelle que soit votre préférence, nous fournirons le bon serveur avec les bons outils à votre entreprise',
-        'Network engineering' => 'Ingénierie réseau',
-        'We know very well that the network is the foundation of digitalisation and you can trust us to manage its various aspects.' => 'Nous savons très bien que le réseau est le fondement de la digitalisation et vous pouvez nous faire confiance pour gérer ses différents aspects.',
-        // CTA
-        'Are you interested in our services ?' => 'Êtes vous intéressé par nos services ?',
-        'Let us know your E-mail and phone number and we will reach out to you shortly' => 'Faites-nous savoir votre e-mail et votre numéro de téléphone et nous vous contacterons bientôt',
-        'Or send us a detailed message' => 'Ou envoyez-nous un message détaillé',
-        // projects
-        'Our Featured applications' => 'Nos applications vedettes',
-        'A Progressive Web App that aims at becoming an essential toolkit for the network engineers by handling the IP operations flawlessly' => 'est une application Web progressive qui vise à devenir une boîte à outils essentielle pour les ingénieurs réseau en gérant les opérations IP sans failles',
-        'A multi-user home automation IOT platform that enables functionalities like realtime sensors monitoring and remote actions' => 'Une plateforme IOT domotique multi-utilisateurs qui permet la surveillance des capteurs en temps réel et les actions à distance',
-        'Tailored according to the algerian education system and destined to be in charge of administering students registrations step by step in privet schools' => 'Adapté au système éducatif algérien et destiné à gérer pas à pas les inscriptions des élèves dans les écoles privées',
-        'Helps real estate agencies keep records of their apartments and clients and fully manages the sales process' => "Aide les agences immobilières à tenir des registres de leurs appartements et de leurs clients et gére entièrement le processus de vente",
-        // contact page
-        "Contact us" => "Contactez nous",
-        "Let's talk" => "Parlez nous",
-        "Let us know how we can help and we'll get in touch with you shortly" => "Faites-nous savoir comment nous pouvons vous aider et nous vous contacterons sous peu",
     ],
 ];
